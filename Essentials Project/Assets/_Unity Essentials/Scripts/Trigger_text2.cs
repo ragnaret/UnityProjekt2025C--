f9f06d6ccbbe9e4c1d830b1dbc0370f4e81d0b5f
KarolHuget <karolhuget.5@gmail.com>
2026-01-22 00:39:22 +0100

    dodanie triggerów

---
+++ Essentials Project/Assets/_Unity Essentials/Scripts/Trigger_text2.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+public class Trigger_text2 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TextMeshProUGUI text;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(false);
+        }
+    }
+}
