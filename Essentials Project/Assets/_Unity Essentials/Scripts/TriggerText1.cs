29ec70111313027a8da4b2e09721721a8cc07285
ragnaret <elelenedh@gmail.com>
2026-01-22 00:09:16 +0100

    Added 3 different trigger boxes and adjusted text, placement and other properties

---
+++ Essentials Project/Assets/_Unity Essentials/Scripts/TriggerText1.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro; 
+
+
+public class TriggerText1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TextMeshProUGUI text;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(false);
+        }
+    }
+}
